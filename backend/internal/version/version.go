d6f9c950114087563fed6eece36a18fe5b6f35b9
cyber-mule <higanbana986@gmail.com>
2026-07-31 13:18:53 +0800

    chore: release v0.0.1-dev.6

--- backend/internal/version/version.go
+++ backend/internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // Build-time variables can be replaced via -ldflags.
 var (
-	Version   = "v0.0.1-dev.5"
+	Version   = "v0.0.1-dev.6"
 	Commit    = "dev"
 	BuildTime = "1970-01-01T00:00:00Z"
 )
