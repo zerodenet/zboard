0c217fc334e02074a56afd304999070603c230fc
kovawx <kovawx@gmail.com>
2026-09-21 10:58:00 +0800

    chore: release v0.0.2-rc.202609210257

--- backend/internal/version/version.go
+++ backend/internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // Build-time variables can be replaced via -ldflags.
 var (
-	Version   = "v0.0.2-rc.202609210252"
+	Version   = "v0.0.2-rc.202609210257"
 	Commit    = "dev"
 	BuildTime = "1970-01-01T00:00:00Z"
 )
